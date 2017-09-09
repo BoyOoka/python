96885675f7009db1261121b17c373ec8dc0ef573
gaya <gaya@192.168.1.129>
2017-09-09 16:35:33 +0800

    1

---
+++ python/src/python/hello.py
@@ -0,0 +1,14 @@
+#ecoding:utf-8
+'''
+Created on 2017年9月9日
+
+@author: gaya
+'''
+from selenium import webdriver
+
+print "Hello world!"
+driver = webdriver.Chrome("/Users/gaya/Documents/workspace/python/chromedriver")
+driver.get("http://www.baidu.com")
+driver.find_element_by_id("kw").send_keys("test")
+driver.find_element_by_id("su").click()
+driver.quit()

a79e44acb36b08fcbde856bb92d83c4c5bfe0800
gaya <gaya@192.168.1.120>
2017-09-11 19:26:06 +0800

    0

---
+++ python/src/python/print.py
@@ -0,0 +1,19 @@
+#ecoding:utf-8
+'''
+Created on 2017年9月11日
+
+@author: gaya
+'''
+print 6+7
+print (1+7)/3
+print "Hello World!"
+print "Hello World!"+str(456)
+a = "hey man~ what's up"
+b = 12345
+c = "1234" + str(456)
+print a , c, b
+print "yes",123
+print int(False)#强制转换为整数
+print float(39)#强制转换为小数
+print bool(21)#强制转换为布尔型
+print bool(0)#强制转换为布尔型
